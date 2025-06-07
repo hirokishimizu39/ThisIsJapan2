0454aff3b41e1d8eeacffbf1ee88e7df03be7dca
Hiroki Shimizu <hiroki71027923@icloud.com>
2025-06-07 16:02:12 +0900

    dbをsqliteからpostgreに移行

--- backend/thisisjapan_api/settings.py
+++ backend/thisisjapan_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,10 +99,12 @@ WSGI_APPLICATION = 'thisisjapan_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get(
+            'DATABASE_URL', 
+            'postgres://postgres:postgres@db:5432/thisisjapan'
+        )
+    )
 }
 
 
